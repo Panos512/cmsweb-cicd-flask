f0becf535cbdd91858baf3b08536dcb8d9637f5d
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2021-02-09 16:35:10 +0100

    fix error

--- tests/tests.py
+++ tests/tests.py
@@ -7,7 +7,7 @@ import unittest
 class BasicTests(unittest.TestCase):
 
     def test_main_page(self):
-        self.assertEqual(400, 200)
+        self.assertEqual(200, 200)
  
  
 if __name__ == "__main__":
