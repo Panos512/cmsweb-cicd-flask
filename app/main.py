212063bdf8371d78ca46b3ce6ef44604ad20898f
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2021-02-16 12:05:42 +0100

    removve whitespace

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'The current tag is: ' + os.environ.get('RELEASE_TAG', 'Not Found')
+    return 'The current tag is:' + os.environ.get('RELEASE_TAG', 'Not Found')
 
 if __name__ == "__main__":
     # Only for debugging while developing
